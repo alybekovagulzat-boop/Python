6ef349417c5f0d6080d4d2087edf134d2a75dfdf
Gulzat <alybekova.gulzat@gmail.com>
2025-10-23 20:27:18 +0600

    two new files

---
+++ firstLesson.py
@@ -0,0 +1,37 @@
+# print(2+2)
+# print("Привет, мир!")
+# name="Алиса"
+# age=25
+# print(name, age)
+# print(name, age, sep="|")
+# print("Строка 1", end="!")
+# print("Строка 1", end=" ")
+# print("Строка 2")
+# print(f"Меня зовут {name} и мне {age} лет")
+# numbers = [1, 2, 3]
+# print("Список:", numbers)
+# print("Число:", 42)
+# print("Булево значение:", True)
+# print("Яблоко", "Банан", "Апельсин", sep="->")
+# print("A", end=" ")
+# print("B", end=" ")
+# print("C", end=" ") 
+# 
+# Третий урок. Тема Hello world
+
+a = int(21)
+day = 21
+month =("октябрь")
+year = 2025
+print(day)
+image1 = ("Приветствую, дорогой гость!")
+print(image1)
+image2 = ("Вы обратились в нашу компанию!", "Мы рады вас услышать.")
+question1 = input("Как вас зовут?")
+print("Приятно познакомиться", question1)
+question2 = input("Сколько вам лет?")
+print(question2)
+image3 = ("Вам мы можем порекомендовать следующие виды тарифов")
+print(image3)
+a = float (2.1)
+print(a)
