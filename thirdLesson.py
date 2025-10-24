6c23bf42fa567106ad39a49d3d458b1c1950e1b1
Gulzat <alybekova.gulzat@gmail.com>
2025-10-24 21:08:07 +0600

    friday commit

---
+++ thirdLesson.py
@@ -0,0 +1,71 @@
+# products = list('apple', 'potato', 'cucumber')
+# print(products)
+# person1 =input("Do you want to buy all this products?")
+
+
+
+# price1 = int(input("enter price"))
+# print("change is", 500-price1)
+# price2 = int (input ('enter price'))
+# # print("change is", 500-int(price1)) это упражнение на числа
+# products = list()
+# products = ['apple', 'potato', 'cucumber']
+# print(products)
+# product =input("Do you want to buy all this products?")
+# print("You bought product", product)
+# print(products[-1])
+
+
+# num = 5/2
+# print(num)
+# num2 = 5//2
+# print(num2)
+# num3 = 109%10
+# print(num3)
+# num5 = 3*3
+# print(num3)
+# num6 = 4**2
+# print(num6)
+
+# a = 5
+# b = 2
+# c = 10
+# print(a * b / c)
+
+# a = 2
+# b = 3
+# c = 1
+# print(a * (b - c))
+# a1 = 5
+# b1 = 1
+# print((a1 - b1) * (a1 + b1))
+# a2 = 0.3
+# b2 = 4
+# c2 = 1
+# print(a2 * (b2 - c2))
+# a3 = 91
+# b3 = 1
+# c3 = 2
+# print((a3 - b3) / (c3 + b3))
+# a4 = 5
+# b4 = 4
+# c4 = 3
+# print(((a4 * b4) / c4) ** 0.5)
+# a5 = 1
+# b5 = 3
+# c5 = 5
+# d5 = 0.2
+# e5 = 0.001
+# f5 = 91
+# print((a5/b5 + (c5 * ((d5 - e5) / f5))))
+# a6 = 0.16
+# b6 = 3.2
+# c6 = 3
+# d6 = 40
+# e6 = 2
+# f6 = 11
+# g6 = 4.125
+# h6 = 4
+# i6 = 5
+# j6 = 1
+# k6 = 6
