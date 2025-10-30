51e10d83c10b7ed57cc06486b7a127242b100c2e
Gulzat <alybekova.gulzat@gmail.com>
2025-10-30 18:21:29 +0600

    sixthLesson

--- sixthLesson.py
+++ sixthLesson.py
@@ -4,6 +4,111 @@
 # else:
 #     print("нечетное") проверка числа на четное и нечетное
 
+# num1 =input("Введите делитель:")
+# num2 =input("Введите делимое:")
+# try:
+#     num1 = int(num1)
+#     num2 = int(num2)
+#     num3 = num1 / num2
+#     print("Результат деления:", num3)
+#     if num3 <= 5:
+#         print("num3 меньше 5")
+#     elif num3 > 5 and num3 < 10:
+#         print("num3 больше 5")
+#     else:
+#         print("num3 больше или равно 10")
+# except:
+#     print(" Введите корректные числа")
+# finally:
+#     print("Блок try завершил выполнение")
+# print("Завершение программы")
 
+
+# a = 5
+# b = 6
+# sum1 = a+b
+# print(sum1)
+
+# word = 'people'
+# text = "Hello, my name is Sam"
+# list1 = [2, 4, 64, 6, 'name']
+
+# search = 'name'
+# if search in text:
+#     print(f"{search} is in the text")
+# else:
+#     print(f'{search} is not in the text')
+# if search in list1:
+#     print(f'{search} is in list1')
+# else:
+#     print(f'{search} is not in the list1')
+
+# text3 = 'lesson5'
+# slice = text3[::2]
+# print(slice)
+
+# text3 = 'lesson5'
+# new_text = text3.upper()
+# print(new_text)
+
+# text3 = 'lesson5'
+# new_text = text3.capitalize()
+# print(new_text)
+
+# word = 'milk'
+# list1 = ['apple', 'banana', 'milk']
+# if word in list1:
+#    new_word = word.replace('k', 'che')
+#    print(new_word)
+
+# name = "aLITA"
+# name2 = name.swapcase()
+# print(name2)
+
+# name = " aLITA "
+# print(len(name))
+# name2 = name.strip()
+# print(name2)
+# print(len(name2))  считывает пробелы и удаляет пробелы
+
+# name = "aLITA sddf fgshghah"
+# name2 = name.split()
+# print(name2)            разделяет каждое слово
+
+# list1 = ['apple', 'banana', 'milk']
+# print(len(list1))                 считает количество элементов
+
+
+# word = 'python'
+# list1 = ['aaple', 'banan', 'milk']
+# print(len(word))   считает кол-во символов
+
+
+
+# Seminar3
+
+# a = 32
+# b = 3.5
+# x = True
+
+# a = str(a)
+# b = str(b)
+# x = str(x)
+
+
+# str_ = input('Enter something')
+# print(str_)
+
+# name = input("Write your name:")
+# print(name)
+# surname = input("Write your surname:")
+# print(surname)  проверить. Не получилось
+
+# name = input("Write your name:")
+# print(name)
+# surname = input("Write your surname:")
+# print(surname)
+# res = name + ' ' + surname
+# print(res)   Упражнение 4
 
 
